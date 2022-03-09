ab117192a3b36fba82009799cee3b09a32eb0124
Thomas Ciszek <tciszek515@gmail.com>
2022-03-09 15:05:23 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -46,11 +46,45 @@ while (enemyHealth > 0 && playerHealth > 0) {
     } //end of while loop
 } // end of function
 
-for (var i = 0; i < enemyNames.length; i++) {
-    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    debugger;
-    fight(pickedEnemyName);
+var startGame = function() {
+    
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    
+    for (var i = 0; i < enemyNames.length; i++) {
+    
+        if (playerHealth > 0){
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
 
+        else{
+            window.alert("GAME OVER");
+            break;
+        }
+    }
+    endGame();
 }
+
+var endGame = function() {
+    
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    } 
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+        if (playAgainConfirm) {
+            startGame();
+        } 
+        else {
+            window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        }
+}
+    startGame();
