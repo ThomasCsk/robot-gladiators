ef72d97f85ea718f4d3c6405e2180babf4e966f2
Thomas Ciszek <tciszek515@gmail.com>
2022-03-12 15:51:27 -0500

    added and easier numeric option for shop selections

--- assets/js/game.js
+++ assets/js/game.js
@@ -97,22 +97,17 @@ var endGame = function() { // ENDGAME FUNCTION
 
 var shop = function() { // SHOP FUNCTION
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "Refill":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
         break;
-        case "UPGRADE":
-        case "Upgrade":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
         break;
-        case "LEAVE":
-        case "Leave":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
         break;
         default:
